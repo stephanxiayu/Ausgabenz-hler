7a569579e8cea84aefdbfb80413f172a848f993e
stephanxiayu <Stephan_Granzow@web.de>
2021-09-19 21:11:09 +0200

    Changes to be committed: 	modified:   lib/chart.dart 	modified:   lib/chart_bar.dart

--- lib/chart.dart
+++ lib/chart.dart
@@ -39,16 +39,22 @@ class Cart extends StatelessWidget {
     return Card(
       elevation: 9,
       margin: EdgeInsets.all(10),
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: groupTransactionValues.map((data) {
-            return ChartBar(
-                data['day'],
-                data['amount'],
-                totalSpending == 0.0
-                    ? 0.0
-                    : (data['amount'] as double) / totalSpending);
-          }).toList()),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupTransactionValues.map((data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                    data['day'],
+                    data['amount'],
+                    totalSpending == 0.0
+                        ? 0.0
+                        : (data['amount'] as double) / totalSpending),
+              );
+            }).toList()),
+      ),
     );
   }
 }
